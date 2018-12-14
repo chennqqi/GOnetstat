9c4b683bb9eeb12301f8e0959a5fde4e2c430bfa
chennqqi <chennqqi@qq.com>
2018-12-14 11:16:37 +0800

    add benchmark

---
+++ gonetstat_test.go
@@ -0,0 +1,22 @@
+package GOnetstat
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTcp(t *testing.T) {
+	p, err := Tcp()
+	assert.Nil(t, err)
+
+	for i, v := range p {
+		t.Logf("%d -> %#v", i, v)
+	}
+}
+
+func BenchmarkTcp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Tcp()
+	}
+}
